396b27c19c16c54d2d338c9bf43ebdbdf65685d4
Shenae Simmons <shhnae@gmail.com>
2019-03-22 16:37:31 -0400

    Real talk, Kenny showed us how to build a deck.

--- main.js
+++ main.js
@@ -4,18 +4,27 @@ const deck = [];
 let player1Card = null;
 let player2Card = null;
 
-function buildDeck() {
+function buildDeck(arr1, arr2) {
+	for(let i = 0; i < values.length; i++){
+		for(let j = 0; j < suits.length; j ++) {
+			let card = {num: arr1[i], suit: arr2[j], value: i}
+			deck.push(card);
+		}
+	}
+	console.log(deck);
+	return deck;
 }
-
+//write function that jack = 10 etc
+//math.random or shuffle deck to give cards
 function dealCardsToPlayers() {
 }
-
+//Player 1 got card Player 2 got card
 function announceCards() {
 }
 
 function cardToRank(card) {
 }
-
+//which player wins
 function announceWinner() {
 }
 
@@ -29,5 +38,5 @@ function playGame() {
   returnCardsToDeck();
 }
 
-buildDeck();
+buildDeck(values, suits);
 playGame();
